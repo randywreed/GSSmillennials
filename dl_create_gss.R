35c88d531207ad4a441445dd6f17d5039ec5ac42
randywreed <randywreed@gmail.com>
2014-07-29 23:55:36 -0400

    updated with info necessary to run

--- dl_create_gss.R
+++ dl_create_gss.R
@@ -6,6 +6,11 @@
 # download link at directlinkupload.com
 download_link<- "http://www.directlinkupload.com/uploads/71.81.255.26/gss1972-2012por.zip"
 #
+# This process takes a large amount of memory. I failed on a 3.5 and 7 gb virtual machine
+# In the end it required a m3:xlarge machine with 15gb ram to run successfully
+# The AMI was acquired at Louis aslet site
+# http://www.louisaslett.com/RStudio_AMI/
+#
 #download file logic from
 ## anthony joseph damico
 # ajdamico@gmail.com
@@ -82,6 +87,7 @@ print( fn[1] )
 
 
 # convert the stata (.dta) file saved on the local disk (at 'fn') into an r data frame
+
 GSS.2012.CS.df <- as.data.set(spss.portable.file( fn[1] ))
 
 
@@ -95,9 +101,10 @@ save( GSS.2012.CS.df , file = "GSS.2012.CS.rda" )
 
 
 unique(GSS.2012.CS.df$year)
-unique(gss2010_12$year)
+#unique(gss2010_12$year)
+gss2008_12<-subset(GSS.2012.CS.df, year > 2007)
+#gss2008_12<-GSS.2012.CS.df
 names(gss2008_12)
-gss2008_12<-subset(GSS.2012.CS.df, year %in% c("2008","2010","2012"))
 relnames<-grep("reli",names(gss2008_12))
 names(gss2008_12[relnames])
 names(gss2008_12[grep("premars",names(gss2008_12))])
